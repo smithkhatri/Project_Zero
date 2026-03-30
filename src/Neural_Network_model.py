265814e284a06c9f46232fb23bafc86b38bc3608
Smith Khatri <smithkhatri9@gmail.com>
2026-03-30 13:26:56 -0400

    incomplete NN model

---
+++ src/Neural_Network_model.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from pathlib import Path
+import torch
+from torch import nn
+import torch.optim as optim
+
+project_root = Path(__file__).resolve().parent.parent
+
+df1 = pd.read_csv(project_root / "data" / "processed" / "TSLA_features.csv")
+df2 = pd.read_csv(project_root / "data" / "processed" / "AAPL_features.csv")
+df3 = pd.read_csv(project_root / "data" / "processed" / "NVDA_features.csv")
+
+
+def split_data(df):
+    X = df[["Close", "High", "Low", "Open", "Volume", "Return", "MA_5", "MA_20", "Volatility"]] #9 features
+    y = df["Target"]
+
+    l = int(len(df)*0.8)
+    
+
+    X_train = X.iloc[:l]
+
+
+    y_train = y.iloc[:l]
+
+    X_test = X.iloc[l:]
+    y_test = y.iloc[l:]
+
+    return X_train, y_train, X_test, y_test
+
+X_train, y_train, X1_test, y1_test = split_data(df1)
+
+
+# class NeuralNetwork(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+
+#         self.layer1 = nn.Linear(9, 64)
+#         self.layer2 = nn.Linear(64, 32)
+#         self.layer3 = nn.Linear(32, 1)
